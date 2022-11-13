e98fdf598357a7ff7a1410245f7fb0a779edc893
cristiano izzo <cristianizzo@me.com>
2022-11-13 05:19:51 +0100

    menu ux profile market

--- test/checkLanguages.js
+++ test/checkLanguages.js
@@ -4,7 +4,6 @@ const jsonlint = require('jsonlint');
 const jsonDiff = require('json-diff');
 
 const JsonTest = {
-
   baseFile: 'en.json',
   files: [],
 
@@ -13,9 +12,11 @@ const JsonTest = {
   },
 
   compareKeys(baseLan, otherLang) {
-
-    return jsonDiff.diff(JSON.parse(baseLan.content), JSON.parse(otherLang.content), {keysOnly: true});
-
+    return jsonDiff.diff(
+      JSON.parse(baseLan.content),
+      JSON.parse(otherLang.content),
+      { keysOnly: true }
+    );
   },
 
   lint(content) {
@@ -24,78 +25,79 @@ const JsonTest = {
 
   async getFiles() {
     return new Promise((resolve) => {
-      fs.readdir(path.resolve(__dirname, '../src/assets/i18n'), (err, filenames) => {
-        if (err) {
-          return;
+      fs.readdir(
+        path.resolve(__dirname, '../src/assets/i18n'),
+        (err, filenames) => {
+          if (err) {
+            return;
+          }
+          resolve(filenames);
         }
-        resolve(filenames);
-      });
+      );
     });
   },
 
   async readFile(file) {
     return new Promise(async (resolve) => {
-      fs.readFile(path.resolve(__dirname, '../src/assets/i18n/' + file), 'utf-8', (err, content) => {
-        if (err) {
-          return;
+      fs.readFile(
+        path.resolve(__dirname, '../src/assets/i18n/' + file),
+        'utf-8',
+        (err, content) => {
+          if (err) {
+            return;
+          }
+          resolve(content);
         }
-        resolve(content);
-      });
+      );
     });
   },
 
   async lintFiles() {
-
     const files = await JsonTest.getFiles();
 
-    await Promise.all(files.map(async file => {
+    await Promise.all(
+      files.map(async (file) => {
+        try {
+          const content = await JsonTest.readFile(file);
+          JsonTest.addFile({ name: file, content });
+          JsonTest.lint(content);
+          console.info(`lint is good ${file}`);
 
-      try {
-
-        const content = await JsonTest.readFile(file);
-        JsonTest.addFile({name: file, content});
-        JsonTest.lint(content);
-        console.info(`lint is good ${file}`);
-
-        return true;
-
-      } catch (error) {
-
-        // debug
-        // console.log('file ', file);
-        throw error;
-      }
-    }));
-
+          return true;
+        } catch (error) {
+          // debug
+          // console.log('file ', file);
+          throw error;
+        }
+      })
+    );
   },
 
   validateFile() {
-
-    const baseFile = JsonTest.files.find(w => w.name === JsonTest.baseFile);
-    const filesToCompare = JsonTest.files.filter(w => w.name !== JsonTest.baseFile);
-    const result = filesToCompare.map(w => JsonTest.compareKeys(baseFile, w));
-    return result.filter(w => w !== undefined);
+    const baseFile = JsonTest.files.find((w) => w.name === JsonTest.baseFile);
+    const filesToCompare = JsonTest.files.filter(
+      (w) => w.name !== JsonTest.baseFile
+    );
+    const result = filesToCompare.map((w) => JsonTest.compareKeys(baseFile, w));
+    return result.filter((w) => w !== undefined);
   },
 
   async init() {
-
     await JsonTest.lintFiles();
     const result = JsonTest.validateFile();
 
-    if(result.length > 0) {
+    if (result.length > 0) {
       throw new Error(JSON.stringify(result));
     }
 
     console.info('Languages are good :)');
-  }
-}
+  },
+};
 JsonTest.init();
 
 process.on('unhandledRejection', (reason, p) => {
-
   console.error('Unhandled Rejection at: Promise', p, 'reason:', reason);
   debugger; // eslint-disable-line no-debugger
 
   process.exit(-1); // eslint-disable-line no-process-exit
 });
-
